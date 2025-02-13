310d2260e8f6679042b3c0f94ab773e761ea38cb
asishk890 <asishk890@gmail.com>
2025-02-13 20:10:37 +0530

    add data one more templet driven from

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
 
 export const routes: Routes = [
-    {path: '', component:TemplateDrivenFormComponent}
+    {path: '', component:TemplateDrivenFormComponent},
+    {path: 'form', loadComponent: () => import('./form/form.component').then((c) => c.FormComponent)}
 ];
